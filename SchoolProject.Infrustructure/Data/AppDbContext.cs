a46745d9b8ed1648b219ead71d612eaea85c9274
mohamedelkhateeb <mohamedelkhteeb74@gmail.com>
2023-10-31 23:44:44 +0200

    Add project files.

---
+++ SchoolProject.Infrustructure/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Infrustructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<DepartmentSubject> DepartmentSubject { get; set; }
+        public DbSet<Subjects> Subjects { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+    }
+}
